c0f1a0dc89d387b78769448a05a4be5211946909
LightTab2 <azorro12@gmail.com>
2022-01-03 18:48:09 +0100

    Hotfix JavaDocs

--- src/main/java/pl/put/poznan/checker/app/ScenarioQualityCheckerApplication.java
+++ src/main/java/pl/put/poznan/checker/app/ScenarioQualityCheckerApplication.java
@@ -5,27 +5,35 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
 /**
- * Serce programu w wersji zdalnego API.
+ * Serce programu w wersji <b>zdalnego API</b>.
  *
  * @author I42-Alpha
- * @version 1.0
+ * @version 2.0
  */
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.checker.rest"})
-
-public class ScenarioQualityCheckerApplication {
+public class ScenarioQualityCheckerApplication
+{
     private static final Logger logger = LoggerFactory.getLogger(ScenarioQualityCheckerApplication.class);
 
     /**
-     * Domyslny konstruktor ScenarioQualityCheckerApplication.
-     * */
+     * Domyślny konstruktor <code>ScenarioQualityCheckerApplication</code>.
+     */
     public ScenarioQualityCheckerApplication() {}
+
     /**
-     * Glowna metoda uruchamiana przy starcie programu.
-     * @param args argumenty wiersza polecen
-     * */
-    public static void main(String[] args) {
-        logger.info("main(String[]) wystartowal");
+     * Główna metoda uruchamiana przy starcie programu.
+     * @param args argumenty wiersza poleceń
+     */
+    public static void main(String[] args)
+    {
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out), true, StandardCharsets.UTF_8));
+        logger.info("Program wystartował.");
         SpringApplication.run(ScenarioQualityCheckerApplication.class, args);
     }
 }
